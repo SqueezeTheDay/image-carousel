666e4c449d2cdb45393ce0ffd107471320b20ce0
Omar Rodriguez <omar@Omars-MacBook-Air.local>
2018-06-26 12:51:13 -0700

    started building crud funtions for original mongoose implementation

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ app.use(cors());
 
 app.use('/:id', express.static('./public'));
 
+
+//read
 app.get('/images/:id', (req, res) => {
   const { id } = req.params;
   const result = [];
@@ -27,8 +29,27 @@ app.get('/images/:id', (req, res) => {
     });
 });
 
-app.post('/products/:id/images', (req, res) => {
+// create
+app.post('images/', (req, res) => {
+  // add new product item
+  // add new images
+  Images.create(req.body, (err, data) => {
+    if (err) throw new Error(err);
+  })
 
+});
+
+
+// update
+app.put('images/:id', (req, res) => {
+  // add the id to the request
+});
+
+// delete
+app.delete('images/:id', (req, res) => {
+  // delete the element at the given ID
+  const { id } = req.params;
+  Images.deleteOne({ _id: id });
 
 });
 
