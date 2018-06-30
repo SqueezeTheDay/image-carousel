fe068ca61e026485ee81805dd7f58bdf37a3a093
Omar Rodriguez <omar@Omars-MacBook-Air.local>
2018-06-30 11:34:16 -0700

    Create, Read and Delete requests have been written

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
+require('newrelic');
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const pgp = require('pg-promise')();
+
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 const db = pgp('postgres://localhost:5432/lululemon');
 
@@ -33,28 +37,54 @@ app.get('/images/:id', (req, res) => {
           { url: '' },
           { url: '' }],
       };
-
-      images.urls[0].url = data[0].url_one;
-      images.urls[1].url = data[0].url_two;
-      images.urls[2].url = data[0].url_three;
-      images.urls[3].url = data[0].url_four;
-      images.urls[4].url = data[0].url_five;
-      result.push(images);
-      res.end(JSON.stringify(result));
+      if (data[0]) {
+        images.urls[0].url = data[0].url_one;
+        images.urls[1].url = data[0].url_two;
+        images.urls[2].url = data[0].url_three;
+        images.urls[3].url = data[0].url_four;
+        images.urls[4].url = data[0].url_five;
+        result.push(images);
+        res.end(JSON.stringify(result));
+      }
+      res.end();
+    })
+    .catch((error) => {
+      console.log('ERROR:', error);
     });
 });
 
-//update
-//create
+// update
 
 
-//delete
+// create
+app.post('/images/', (req, res) => {
+
+  db.none(`INSERT INTO image_urls VALUES(
+       '${req.body.id}',
+       '${req.body.url_one}',
+       '${req.body.url_two}',
+       '${req.body.url_three}',
+       '${req.body.url_four}',
+       '${req.body.url_five}'
+     )`)
+    .then(() => {
+      console.log('items have been added to the database')
+      res.end();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  res.end();
+});
+
+// delete
 app.delete('/images/:id', (req, res) =>{
   const { id } = req.params;
-  console.log('that was a delete!')
   db.result(`DELETE FROM image_urls WHERE id = ${id};`)
     .then((result) => {
-      console.log(result.rowCount);
+      console.log(result.rowCount + ' items have been deleted');
+      res.end();
     })
     .catch((error) => {
       console.log('ERROR:', error);
